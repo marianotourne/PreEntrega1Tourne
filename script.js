f8859800827ed67b1818b3759c487c1177bae2c5
Mariano Tourné <marianotourne@gmail.com>
2022-12-26 13:46:31 -0300

    Unifiqué opcion y seleccion en una funcion, y mejore funcion de programa principal

--- script.js
+++ script.js
@@ -1,88 +1,60 @@
-// DEFINO VARIABLES GLOBALES
-let opcionJugador1;
-let opcionJugador2;
-
-let seleccion1;
-let seleccion2;
-
-// FUNCION PARA ALMACENAR OPCION ELEGIDA POR AMBOS USUARIOS
-function preguntarOpcion() {
-  opcionJugador1 = Number(
+// PREGUNTA AL USUARIO OPCION A ELEGIR Y DEVUELVE RESULTADO ELEGIDO EN PALABRAS
+function obtenerOpcion(nombreJugador) {
+  let opcion = Number(
     prompt(
-      "Seleccione opción para el jugador 1:\n1: Piedra\n2: Papel\n3: Tijera"
+      `Seleccione opción para el ${nombreJugador}:\n1: Piedra\n2: Papel\n3: Tijera`
     )
   );
-  opcionJugador2 = Number(
-    prompt(
-      "Seleccione opción para el jugador 2:\n1: Piedra\n2: Papel\n3: Tijera"
-    )
-  );
-}
 
-// FUNCION PARA ASIGNAR SELECCION DE USUARIO 1
-function seleccionJugadorUno() {
-  switch (true) {
-    case opcionJugador1 == 1:
-      seleccion1 = "Piedra";
+  let opcionElegida;
+  switch (opcion) {
+    case 1:
+      opcionElegida = "Piedra";
       break;
-    case opcionJugador1 == 2:
-      seleccion1 = "Papel";
+    case 2:
+      opcionElegida = "Papel";
       break;
-    case opcionJugador1 == 3:
-      seleccion1 = "Tijera";
+    case 3:
+      opcionElegida = "Tijera";
       break;
     default:
-      seleccion1 = "ERROR";
-      break;
+      opcionElegida = "ERROR";
   }
+
+  return opcionElegida;
 }
 
-// FUNCION PARA ASIGNAR SELECCION DE USUARIO 2
-function seleccionJugadorDos() {
-  switch (true) {
-    case opcionJugador2 == 1:
-      seleccion2 = "Piedra";
-      break;
-    case opcionJugador2 == 2:
-      seleccion2 = "Papel";
-      break;
-    case opcionJugador2 == 3:
-      seleccion2 = "Tijera";
-      break;
-    default:
-      seleccion2 = "ERROR";
-      break;
-  }
-}
-
-// PROGRAMA PRINCIPAL
-function jugar() {
-  let resultado;
-  preguntarOpcion();
-  seleccionJugadorUno(opcionJugador1);
-  seleccionJugadorDos(opcionJugador2);
-  if (seleccion1 == "ERROR" || seleccion2 == "ERROR") {
-    return "No se puede calcular el resultado porque ingresaron alguna opción incorrecta";
+// RECIBE VALORES INGRESADOS POR JUGADORES Y CALCULA EL RESULTADO
+function calcularResultado(opcionJugador1, opcionJugador2) {
+  let resultado = "";
+  if (opcionJugador1 === "ERROR" || opcionJugador2 === "ERROR") {
+    resultado =
+      "No se puede calcular el resultado porque ingresaron alguna opción incorrecta";
   } else if (opcionJugador1 === opcionJugador2) {
-    return (resultado = "Empataron");
+    resultado = "Empataron";
   } else if (
-    (opcionJugador1 == 1 && opcionJugador2 == 3) ||
-    (opcionJugador1 == 2 && opcionJugador2 == 1) ||
-    (opcionJugador1 == 3 && opcionJugador2 == 2)
+    (opcionJugador1 === "Piedra" && opcionJugador2 === "Tijera") ||
+    (opcionJugador1 === "Papel" && opcionJugador2 === "Piedra") ||
+    (opcionJugador1 === "Tijera" && opcionJugador2 === "Papel")
   ) {
-    return (resultado =
-      "Ganó el jugador 1: " + seleccion1 + " le gana a " + seleccion2);
+    resultado = `Ganó el Jugador 1: ${opcionJugador1} le gana a ${opcionJugador2}`;
   } else {
-    return (resultado =
-      "Ganó el jugador 2: " + seleccion2 + " le gana a " + seleccion1);
+    resultado = `Ganó el Jugador 2: ${opcionJugador2} le gana a ${opcionJugador1}`;
   }
+
+  return resultado;
 }
 
-// FUNCION PARA REPETIR EL JUEGO O SALIR
-function repetir() {
-  let respuesta;
+//PROGRAMA PRINCIPAL
+function main() {
+  let respuesta = 1;
   do {
-    alert(jugar());
+    const opcionJugador1 = obtenerOpcion("Jugador 1");
+    const opcionJugador2 = obtenerOpcion("Jugador 2");
+
+    const resultado = calcularResultado(opcionJugador1, opcionJugador2);
+    alert(resultado);
+
     respuesta = Number(
       prompt(
         "Si querés volver a jugar presioná el número 1, caso contrario presioná cualquier tecla"
@@ -91,4 +63,4 @@ function repetir() {
   } while (respuesta == 1);
 }
 
-repetir();
+main();
